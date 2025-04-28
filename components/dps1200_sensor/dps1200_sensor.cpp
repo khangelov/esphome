1c358afa19d88a8ecf1748545a63b53ded95d50a
khangelov <40702364+khangelov@users.noreply.github.com>
2025-04-29 00:34:24 +0300

    a

--- components/dps1200_sensor/dps1200_sensor.cpp
+++ components/dps1200_sensor/dps1200_sensor.cpp
@@ -1,48 +1,34 @@
-#include "dps1200_sensor.h"
-#include "esphome/core/log.h"
+// esphome/components/dps1200_sensor/dps1200_sensor.h
+#pragma once
+#include "esphome/core/component.h"
+#include "esphome/components/sensor/sensor.h"
 
 namespace esphome {
 namespace dps1200_sensor {
+class DPS1200Sensor : public Component {
+ public:
+  void setup() override;
+  void loop() override;
+  float get_setup_priority() const override { return setup_priority::DATA; }
 
-static const char *const TAG = "dps1200_sensor";
+  void set_volt_in_sensor(sensor::Sensor *sensor) { volt_in_ = sensor; }
+  void set_amp_in_sensor(sensor::Sensor *sensor) { amp_in_ = sensor; }
+  void set_watt_in_sensor(sensor::Sensor *sensor) { watt_in_ = sensor; }
+  void set_volt_out_sensor(sensor::Sensor *sensor) { volt_out_ = sensor; }
+  void set_amp_out_sensor(sensor::Sensor *sensor) { amp_out_ = sensor; }
+  void set_watt_out_sensor(sensor::Sensor *sensor) { watt_out_ = sensor; }
+  void set_internal_temp_sensor(sensor::Sensor *sensor) { internal_temp_ = sensor; }
+  void set_fan_rpm_sensor(sensor::Sensor *sensor) { fan_rpm_ = sensor; }
 
-void DPS1200Sensor::setup() {
-  ESP_LOGCONFIG(TAG, "Setting up DPS1200 Sensor...");
-}
-
-void DPS1200Sensor::update() {
-  // Simulated example (replace with real reads)
-  float vin = 230.0;
-  float ain = 5.0;
-  float vout = 12.2;
-  float aout = 100.0;
-  float temp = 45.0;
-  float rpm = 3200.0;
-
-  float win = vin * ain;
-  float wout = vout * aout;
-
-  if (volt_in_sensor) volt_in_sensor->publish_state(vin);
-  if (amp_in_sensor) amp_in_sensor->publish_state(ain);
-  if (watt_in_sensor) watt_in_sensor->publish_state(win);
-  if (volt_out_sensor) volt_out_sensor->publish_state(vout);
-  if (amp_out_sensor) amp_out_sensor->publish_state(aout);
-  if (watt_out_sensor) watt_out_sensor->publish_state(wout);
-  if (internal_temp_sensor) internal_temp_sensor->publish_state(temp);
-  if (fan_rpm_sensor) fan_rpm_sensor->publish_state(rpm);
-}
-
-void DPS1200Sensor::dump_config() {
-  ESP_LOGCONFIG(TAG, "DPS1200 Sensor Config:");
-  LOG_SENSOR("  ", "Input Voltage", this->volt_in_sensor);
-  LOG_SENSOR("  ", "Input Current", this->amp_in_sensor);
-  LOG_SENSOR("  ", "Input Power", this->watt_in_sensor);
-  LOG_SENSOR("  ", "Output Voltage", this->volt_out_sensor);
-  LOG_SENSOR("  ", "Output Current", this->amp_out_sensor);
-  LOG_SENSOR("  ", "Output Power", this->watt_out_sensor);
-  LOG_SENSOR("  ", "Internal Temperature", this->internal_temp_sensor);
-  LOG_SENSOR("  ", "Fan RPM", this->fan_rpm_sensor);
-}
-
+ protected:
+  sensor::Sensor *volt_in_{nullptr};
+  sensor::Sensor *amp_in_{nullptr};
+  sensor::Sensor *watt_in_{nullptr};
+  sensor::Sensor *volt_out_{nullptr};
+  sensor::Sensor *amp_out_{nullptr};
+  sensor::Sensor *watt_out_{nullptr};
+  sensor::Sensor *internal_temp_{nullptr};
+  sensor::Sensor *fan_rpm_{nullptr};
+};
 }  // namespace dps1200_sensor
 }  // namespace esphome
