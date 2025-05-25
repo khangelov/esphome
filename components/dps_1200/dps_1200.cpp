9e1748331bc9d243dae47a5dbcaec3c8fda5f25d
khangelov <40702364+khangelov@users.noreply.github.com>
2025-05-25 23:09:09 +0300

    Update dps_1200.cpp

--- components/dps_1200/dps_1200.cpp
+++ components/dps_1200/dps_1200.cpp
@@ -1,5 +1,33 @@
 #include "dps_1200.h"
 #include "esphome/core/log.h"
+// Enable / Disable defines
+
+#define ADJUST_TMP_F 18 // TODO best way to calibrate?
+float intake_tmp_c = 0.0;
+float internal_tmp_c = 0.0;
+
+#ifdef ENADD_STATS
+// additional vals to read
+float volt_in = 0.0;
+float amp_in = 0.0;
+float watt_in = 0.0;
+float volt_out = 0.0;
+float amp_out = 0.0;
+float watt_out = 0.0;
+#endif
+
+
+uint8_t addy = 0x58; //Supply Address verify w/I2C scanner
+uint8_t i; // index for loop
+uint8_t reg[6] = {0x08, 0x0a, 0x0e, 0x10, 0x1c, 0x1e}; 
+uint8_t cs, regCS;
+uint16_t msg[3]; //Received messages from PS
+float ret, Stat; //reused calculated values
+
+float f2c(uint16_t temp) {
+  return (temp- 32) *.5556;
+}
+
 
 namespace esphome {
 namespace dps_1200 {
