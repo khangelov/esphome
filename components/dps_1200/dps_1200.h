a1eac72925b49d9dc461730b4ebea7df2d0e3551
khangelov <40702364+khangelov@users.noreply.github.com>
2025-05-26 17:24:53 +0300

    Update dps_1200.h

--- components/dps_1200/dps_1200.h
+++ components/dps_1200/dps_1200.h
@@ -8,7 +8,7 @@
 namespace esphome {
 namespace dps_1200 {
 
-class HPPSUMonitor : public PollingComponent, public I2CDevice::I2CDevice {
+class HPPSUMonitor : public PollingComponent, public i2c::I2CDevice {
  public:
   sensor::Sensor *vin_sensor{nullptr};
   sensor::Sensor *vout_sensor{nullptr};
