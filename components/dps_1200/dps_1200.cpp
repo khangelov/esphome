a6df8965e21d2ff7bd538493ea96ff4277e2da49
khangelov <40702364+khangelov@users.noreply.github.com>
2025-06-01 11:39:01 -0700

    2

--- components/dps_1200/dps_1200.cpp
+++ components/dps_1200/dps_1200.cpp
@@ -11,7 +11,7 @@ void HPPSUMonitor::setup() {
   ESP_LOGCONFIG(TAG, "Setting up HPPSUMonitor...");
 }
 
-
+/*
 void HPPSUMonitor::update() {
   uint8_t addy = 0x58;  // Address is already set via set_i2c_address, but kept here for clarity
   uint8_t buffer[13];
@@ -47,6 +47,57 @@ void HPPSUMonitor::update() {
       this->temp_sensor->publish_state(NAN);
   }
 }
+*/
+void HPPSUMonitor::update() {
+  float intake_tmp_c = 0.0;
+  float internal_tmp_c = 0.0;
+  float volt_in = 0.0;
+  float amp_in = 0.0;
+  float watt_in = 0.0;
+  float volt_out = 0.0;
+  float amp_out = 0.0;
+  float watt_out = 0.0;
+  uint8_t addy = 0x58; //Supply Address verify w/I2C scanner
+  uint8_t i; // index for loop
+  uint8_t reg[6] = {0x08, 0x0a, 0x0e, 0x10, 0x1c, 0x1e}; 
+  uint8_t cs, regCS;
+  uint16_t msg[3]; //Received messages from PS
+  float ret, Stat; //reused calculated values
+  uint8_t buffer[13];	
+
+	for (i = 0; i<6; i++) {
+ 	   cs = (addy<<1)+ reg[i];
+ 	   regCS = ((0xff - cs) +1 ) & 0xff; //calculate checksum of add+reg
+  	   Wire.beginTransmission(addy); //open i2c with dps1200
+  	   Wire.write(reg[i]); // dps1200 cycle through registers
+  	   Wire.write(regCS); // write checksum each time
+  	   Wire.endTransmission();  // close DPS1200 i2c
+  	   delay(1); // Short delay between operations
+ 
+  	   Wire.requestFrom((int)addy, 3);  
+  	   msg[0] = Wire.read(); msg[1] = Wire.read();msg[2] = Wire.read();
+	   ret = (msg[1] << 8) + msg[0]; // Shift to MSB + LSB
+	  
+	   if ( i == 0)  { Stat = ret / 32 ; // scale factor
+    	   Serial.print ("Grid Voltage = "); Serial.print (Stat);Serial.print ("V   "); vin_sensor->publish_state(Stat); }
+ 	   
+	   if ( i == 1) { Stat = ret / 128 ; // scale factor
+    	   Serial.print ("Grid Current = "); Serial.print (Stat);Serial.print ("A   "); iin_sensor->publish_state(Stat);}
+
+	   if ( i == 2) { Stat = ret / 256; // scale factor
+    	   Serial.print ("Output Voltage = "); Serial.print (Stat);Serial.print ("V   "); vout_sensor->publish_state(Stat);  }
+	  
+	   if ( i == 3) { Stat = ret / 128; // scale factor
+    	   Serial.print ("Output Current = "); Serial.print (Stat);Serial.println ("A  "); iout_sensor->publish_state(Stat); }
+
+	   if ( i == 4) { Stat = ret / 32; // scale factor
+	   Serial.print ("Internal Temp = "); Stat = (Stat - 32.0f) * 5.0f / 9.0f; Serial.print (Stat);Serial.print ("C  ");temp_sensor->publish_state(Stat);}
+       
+	   if ( i == 5) { Stat = ret ; // scale factor = 1
+    	   Serial.print ("Fan RPM = "); Serial.print (Stat);Serial.print (" RPM   ");fan_sensor->publish_state(Stat);}
+
+}
+
 
 }  // namespace dps_1200
 }  // namespace esphome
