63ed2aed48a94362f3c5eaae64bf1f918a941cf0
khangelov <40702364+khangelov@users.noreply.github.com>
2025-06-05 08:10:29 -0700

    v1

--- components/dps_1200/sensor.py
+++ components/dps_1200/sensor.py
@@ -71,7 +71,7 @@ CONFIG_SCHEMA = cv.Schema({
         accuracy_decimals=1,
         device_class=DEVICE_CLASS_POWER
     ),
-}).extend(cv.polling_component_schema("60s")).extend(i2c.i2c_device_schema(0x59))
+}).extend(cv.polling_component_schema("60s")).extend(i2c.i2c_device_schema(0x58))
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
