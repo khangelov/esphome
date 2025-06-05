991eb2c5614ee2009117109b358c3de0a58a87f4
khangelov <40702364+khangelov@users.noreply.github.com>
2025-06-05 06:20:37 -0700

    v1

--- components/dps_1200/dps_1200.h
+++ components/dps_1200/dps_1200.h
@@ -1,28 +1,38 @@
 #pragma once
-
+#include "esphome/components/sensor/sensor.h"
 #include "esphome/components/i2c/i2c.h"
 #include "esphome/core/component.h"
-#include "esphome/core/log.h"
-#include "esphome/components/sensor/sensor.h"
 
 namespace esphome {
-namespace dps_1200 {
+namespace dps1200 {
 
-class HPPSUMonitor : public PollingComponent, public i2c::I2CDevice {
+class DPS1200Sensor : public PollingComponent, public i2c::I2CDevice {
  public:
-  sensor::Sensor *vin_sensor{nullptr};
-  sensor::Sensor *vout_sensor{nullptr};
-  sensor::Sensor *iin_sensor{nullptr};
-  sensor::Sensor *iout_sensor{nullptr};
-  sensor::Sensor *pin_sensor{nullptr};
-  sensor::Sensor *pout_sensor{nullptr};
-  sensor::Sensor *temp_sensor{nullptr};
-  sensor::Sensor *fan_sensor{nullptr};
+  DPS1200Sensor() = default;
 
-  void setup() override;
+  void set_sensors(sensor::Sensor *volt_in, sensor::Sensor *amp_in,
+                   sensor::Sensor *volt_out, sensor::Sensor *amp_out,
+                   sensor::Sensor *internal_temp, sensor::Sensor *fan_rpm) {
+    volt_in_ = volt_in;
+    amp_in_ = amp_in;
+    volt_out_ = volt_out;
+    amp_out_ = amp_out;
+    internal_temp_ = internal_temp;
+    fan_rpm_ = fan_rpm;
+  }
+
   void update() override;
 
+ protected:
+  float f2c(float f) { return (f - 32.0f) * 5.0f / 9.0f; }
+
+  sensor::Sensor *volt_in_{nullptr};
+  sensor::Sensor *amp_in_{nullptr};
+  sensor::Sensor *volt_out_{nullptr};
+  sensor::Sensor *amp_out_{nullptr};
+  sensor::Sensor *internal_temp_{nullptr};
+  sensor::Sensor *fan_rpm_{nullptr};
 };
 
-}  // namespace dps_1200
+}  // namespace dps1200
 }  // namespace esphome
---
+++ components/dps_1200/sensor.py
@@ -0,0 +1,82 @@
+import esphome.codegen as cg
+import esphome.config_validation as cv
+from esphome.components import sensor, i2c
+from esphome.const import (
+    CONF_ID, UNIT_VOLT, UNIT_AMPERE, UNIT_RPM, UNIT_CELSIUS,
+    ICON_FLASH, ICON_CURRENT_AC, ICON_THERMOMETER, ICON_FAN
+)
+
+CONF_GRID_VOLTAGE = "grid_voltage"
+CONF_GRID_CURRENT = "grid_current"
+CONF_OUTPUT_VOLTAGE = "output_voltage"
+CONF_OUTPUT_CURRENT = "output_current"
+CONF_INTERNAL_TEMPERATURE = "internal_temperature"
+CONF_FAN_RPM = "fan_rpm"
+
+dps1200_ns = cg.esphome_ns.namespace("dps1200")
+DPS1200Sensor = dps1200_ns.class_("DPS1200Sensor", cg.PollingComponent, i2c.I2CDevice)
+
+CONFIG_SCHEMA = (
+    cv.Schema({
+        cv.GenerateID(): cv.declare_id(DPS1200Sensor),
+        cv.Optional(CONF_GRID_VOLTAGE): sensor.sensor_schema(
+            unit_of_measurement=UNIT_VOLT,
+            icon=ICON_FLASH,
+            accuracy_decimals=2,
+        ),
+        cv.Optional(CONF_GRID_CURRENT): sensor.sensor_schema(
+            unit_of_measurement=UNIT_AMPERE,
+            icon=ICON_CURRENT_AC,
+            accuracy_decimals=2,
+        ),
+        cv.Optional(CONF_OUTPUT_VOLTAGE): sensor.sensor_schema(
+            unit_of_measurement=UNIT_VOLT,
+            icon=ICON_FLASH,
+            accuracy_decimals=2,
+        ),
+        cv.Optional(CONF_OUTPUT_CURRENT): sensor.sensor_schema(
+            unit_of_measurement=UNIT_AMPERE,
+            icon=ICON_CURRENT_AC,
+            accuracy_decimals=2,
+        ),
+        cv.Optional(CONF_INTERNAL_TEMPERATURE): sensor.sensor_schema(
+            unit_of_measurement=UNIT_CELSIUS,
+            icon=ICON_THERMOMETER,
+            accuracy_decimals=1,
+        ),
+        cv.Optional(CONF_FAN_RPM): sensor.sensor_schema(
+            unit_of_measurement=UNIT_RPM,
+            icon=ICON_FAN,
+            accuracy_decimals=0,
+        ),
+    }).extend(cv.polling_component_schema("10s")).extend(i2c.i2c_device_schema(0x5A))
+)
+
+async def to_code(config):
+    var = cg.new_Pvariable(config[CONF_ID])
+    await cg.register_component(var, config)
+    await i2c.register_i2c_device(var, config)
+
+    if CONF_GRID_VOLTAGE in config:
+        sens = await sensor.new_sensor(config[CONF_GRID_VOLTAGE])
+        cg.add(var.set_sensors(sens, None, None, None, None, None))
+
+    if CONF_GRID_CURRENT in config:
+        sens = await sensor.new_sensor(config[CONF_GRID_CURRENT])
+        cg.add(var.set_sensors(None, sens, None, None, None, None))
+
+    if CONF_OUTPUT_VOLTAGE in config:
+        sens = await sensor.new_sensor(config[CONF_OUTPUT_VOLTAGE])
+        cg.add(var.set_sensors(None, None, sens, None, None, None))
+
+    if CONF_OUTPUT_CURRENT in config:
+        sens = await sensor.new_sensor(config[CONF_OUTPUT_CURRENT])
+        cg.add(var.set_sensors(None, None, None, sens, None, None))
+
+    if CONF_INTERNAL_TEMPERATURE in config:
+        sens = await sensor.new_sensor(config[CONF_INTERNAL_TEMPERATURE])
+        cg.add(var.set_sensors(None, None, None, None, sens, None))
+
+    if CONF_FAN_RPM in config:
+        sens = await sensor.new_sensor(config[CONF_FAN_RPM])
+        cg.add(var.set_sensors(None, None, None, None, None, sens))
