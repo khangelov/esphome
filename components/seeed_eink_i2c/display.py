27f34f0139f2ab357d76b54b55639311d0e79197
khangelov <40702364+khangelov@users.noreply.github.com>
2025-12-25 18:31:40 +0200

    Switch from I2C to SPI for Seeed E-Ink display

--- components/seeed_eink_i2c/display.py
+++ components/seeed_eink_i2c/display.py
@@ -1,27 +1,21 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.components import display, i2c, time
+from esphome.components import display, spi, time
 from esphome.const import CONF_ID
 
-seeed_ns = cg.esphome_ns.namespace("seeed_eink_i2c")
-SeeedEInk = seeed_ns.class_("SeeedEInk", display.Display, i2c.I2CDevice)
+seeed_ns = cg.esphome_ns.namespace("seeed_eink_spi")
+SeeedEInk = seeed_ns.class_("SeeedEInk", display.Display, spi.SPIDevice)
 
 CONF_TIME_ID = "time_id"
 
-CONFIG_SCHEMA = (
-    display.FULL_DISPLAY_SCHEMA.extend(
-        {
-            cv.GenerateID(): cv.declare_id(SeeedEInk),
-            cv.Required(CONF_TIME_ID): cv.use_id(time.RealTimeClock),
-        }
-    )
-    .extend(i2c.i2c_device_schema(0x3C))
-)
+CONFIG_SCHEMA = display.FULL_DISPLAY_SCHEMA.extend({
+    cv.GenerateID(): cv.declare_id(SeeedEInk),
+    cv.Required(CONF_TIME_ID): cv.use_id(time.RealTimeClock),
+}).extend(spi.spi_device_schema())
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await display.register_display(var, config)
-    await i2c.register_i2c_device(var, config)
-
+    await spi.register_spi_device(var, config)
     time_var = await cg.get_variable(config[CONF_TIME_ID])
     cg.add(var.set_time(time_var))
